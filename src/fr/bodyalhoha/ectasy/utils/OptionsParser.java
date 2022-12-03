901730839ce4a45874449d62ddbcb31386d369c1
body <me@body.sh>
2022-12-03 09:47:37 +0100

    join logs

--- src/fr/bodyalhoha/ectasy/utils/OptionsParser.java
+++ src/fr/bodyalhoha/ectasy/utils/OptionsParser.java
@@ -1,21 +1,24 @@
 package fr.bodyalhoha.ectasy.utils;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class OptionsParser {
 
     public HashMap<String, String> args = new HashMap<String, String>();
+    public List<String> bools = new ArrayList<String>();
 
-
-    public OptionsParser(String[] args, String... has){
+    public OptionsParser(String[] args, String[] has, String[] contains){
         for(int i = 0; i < args.length; i++){
-            String name = args[i];
+            String name = args[i].toLowerCase();
             if(name.startsWith("--"))
                 name = name.substring(2);
+            if(Arrays.asList(contains).contains(name)){
+                bools.add(name);
+                continue;
+            }
             if(!Arrays.asList(has).contains(name))
                 continue;
+
             this.args.put(name, args[++i]);
         }
 
@@ -37,6 +40,10 @@ public class OptionsParser {
         }
 
         return def;
+    }
+
+    public boolean getBool(String arg){
+        return bools.contains(arg);
     }
 
 
