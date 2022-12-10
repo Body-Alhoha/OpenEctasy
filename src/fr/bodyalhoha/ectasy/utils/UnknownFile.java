70392294d7f2a2b111e19de00c58c6c662c35a87
InfiniteCod3 <91837651+InfiniteCod3@users.noreply.github.com>
2022-12-10 18:08:30 +0100

    Update UnknownFile.java

--- src/fr/bodyalhoha/ectasy/utils/UnknownFile.java
+++ src/fr/bodyalhoha/ectasy/utils/UnknownFile.java
@@ -5,15 +5,24 @@ import org.apache.commons.io.IOUtils;
 import java.io.InputStream;
 
 public class UnknownFile {
+    // The name of the file
     public String name;
+    // The contents of the file as a byte array
     public byte[] bytes;
 
     public UnknownFile(String name, byte[] bytes) {
         this.name = name;
     }
 
+    /**
+     * Creates an instance of the UnknownFile class from an input stream
+     * @param name the name of the file
+     * @param in the input stream to read the file from
+     * @throws Exception if there is an error reading from the input stream
+     */
     public UnknownFile(String name, InputStream in) throws Exception {
         this.name = name;
+        // Read the input stream and convert it to a byte array
         this.bytes = IOUtils.toByteArray(in);
     }
 }
