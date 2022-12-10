4075b1bb18998edd5f644fa07e3ce69070b1d370
InfiniteCod3 <91837651+InfiniteCod3@users.noreply.github.com>
2022-12-10 18:18:31 +0100

    Update JarLoader.java

--- src/fr/bodyalhoha/ectasy/utils/JarLoader.java
+++ src/fr/bodyalhoha/ectasy/utils/JarLoader.java
@@ -15,19 +15,52 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.ClassNode;
 
+/**
+ * This class is a utility for working with JAR files.
+ */
 public class JarLoader {
 
+    /**
+     * A list of ClassNode objects that have been parsed from the input JAR file.
+     */
     public List<ClassNode> classes = new ArrayList<ClassNode>();
+
+    /**
+     * A list of ClassNode objects that have been added after the input JAR file was loaded.
+     */
     public List<ClassNode> newClasses = new ArrayList<ClassNode>();
+
+    /**
+     * A list of UnknownFile objects that have been parsed from the input JAR file.
+     */
     public List<UnknownFile> files = new ArrayList<UnknownFile>();
+
+    /**
+     * The file path of the input JAR file.
+     */
     public String input;
+
+    /**
+     * The file path of the output JAR file.
+     */
     public String output;
 
+    /**
+     * Constructs a JarLoader instance with the specified input and output JAR files.
+     *
+     * @param input The file path of the input JAR file.
+     * @param output The file path of the output JAR file.
+     */
     public JarLoader(String input,String output) {
         this.input = input;
         this.output = output;
     }
 
+    /**
+     * Loads the input JAR file and parses its contents into a list of ClassNode objects and a list of UnknownFile objects.
+     *
+     * @throws Exception If an error occurs while reading or parsing the input JAR file.
+     */
     public void loadJar() throws Exception{
         File inputFile = new File(input);
         try (ZipInputStream jarInputStream = new ZipInputStream(Files.newInputStream(inputFile.toPath()))) {
@@ -54,6 +87,11 @@ public class JarLoader {
 
     }
 
+    /**
+     * Saves the contents of the input JAR file, along with any additional ClassNode objects and UnknownFile objects that have been added, to the output JAR file.
+     *
+     * @throws Exception If an error occurs while writing to the output JAR file.
+     */
     public void saveJar() throws Exception {
         File outputFile = new File(output);
         try (JarOutputStream out = new JarOutputStream(Files.newOutputStream(outputFile.toPath()))) {
@@ -77,7 +115,13 @@ public class JarLoader {
 
     }
 
+    /**
+     * Adds a ClassNode object to the list of ClassNode objects.
+     *
+     * @param cn The ClassNode object to add.
+     */
     public void addClass(ClassNode cn) {
         this.classes.add(cn);
     }
 }
+
