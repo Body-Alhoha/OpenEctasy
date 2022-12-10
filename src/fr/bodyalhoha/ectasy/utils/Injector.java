25014bfc94b171b8460de7de09ae9a5a936ad6c7
InfiniteCod3 <91837651+InfiniteCod3@users.noreply.github.com>
2022-12-10 18:25:44 +0100

    Update Injector.java

--- src/fr/bodyalhoha/ectasy/utils/Injector.java
+++ src/fr/bodyalhoha/ectasy/utils/Injector.java
@@ -9,8 +9,17 @@ import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * This class is a utility for injecting code into JAR files.
+ */
 public class Injector {
 
+    /**
+     * Decodes the specified URL-encoded path.
+     *
+     * @param path The URL-encoded path to decode.
+     * @return The decoded path.
+     */
     public static String decode(String path){
         try{
             return java.net.URLDecoder.decode(path, StandardCharsets.UTF_8.name());
@@ -19,6 +28,13 @@ public class Injector {
         }
     }
 
+       /**
+     * Injects code into the specified input JAR file and saves the resulting JAR file to the specified output path.
+     *
+     * @param input The file path of the input JAR file.
+     * @param output The file path of the output JAR file.
+     * @param parser An OptionsParser instance containing the options for the code injection.
+     */
     public static void inject(String input, String output, OptionsParser parser){
         JarLoader plugin = new JarLoader(input, output);
         JarLoader current = new JarLoader(decode(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()), "");
@@ -28,11 +44,18 @@ public class Injector {
             current.loadJar();
             plugin.loadJar();
 
+            // Add the API class from the current JAR file to the list of new classes in the plugin JAR file.
             current.classes.stream().filter(cn -> cn.name.contains("API")).forEach(plugin.newClasses::add);
+
+            // Iterate over all classes in the plugin JAR file.
             plugin.classes.forEach((cn) -> {
+                // Iterate over all methods in the current class.
                 cn.methods.forEach((mn) -> {
+                    // If the method is the onEnable method, inject the code.
                     if(mn.name.equalsIgnoreCase("onEnable")){
                         injected.set(true);
+
+                        // Create the instructions for the injected code.
                         InsnList list = new InsnList();
                         list.add(new TypeInsnNode(Opcodes.NEW, "fr/bodyalhoha/ectasy/SpigotAPI"));
                         list.add(new InsnNode(Opcodes.DUP));
@@ -42,28 +65,36 @@ public class Injector {
                         list.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, "fr/bodyalhoha/ectasy/SpigotAPI", "<init>", "(Lorg/bukkit/plugin/java/JavaPlugin;Ljava/lang/String;Z)V", false));
                         list.add(new InsnNode(Opcodes.POP));
 
+                                               // Insert the instructions into the onEnable method.
                         mn.instructions.insertBefore(mn.instructions.getFirst(), list);
                     }
                 });
             });
 
-
+            // If the code was not injected, print an error message and return.
             if(!injected.get()){
                 System.out.println("[-] Could not inject " + plugin.input);
                 return;
             }
+
+            // Save the modified JAR file.
             plugin.saveJar();
 
+            // Print a success message.
             System.out.println("[+] Injected " + plugin.input + " -> " + plugin.output);
-
-
         }catch (Exception e){
             System.out.println("[-] Could not inject " + plugin.input);
             e.printStackTrace();
-
         }
     }
 
+    /**
+     * Injects code into all JAR files in the specified directory and saves the resulting JAR files to the specified output directory.
+     *
+     * @param directory The directory containing the input JAR files.
+     * @param out The directory where the output JAR files will be saved.
+     * @param parser An OptionsParser instance containing the options for the code injection.
+     */
     public static void injectDirectory(File directory, String out, OptionsParser parser){
         File output = new File(out);
         if(!output.exists())
